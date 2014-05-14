8bf31784c76e3ac879e7745a49a4422beb09e9ef
Tom Helmer Hansen <thelmer@thehome.dk>
2014-05-14 19:57:34 +0200

    template added, permissions updated

---
+++ themes/custom/atp_theme/templates/layouts/section_a3/section-a3.tpl.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * @file
+ * This is the section A variant 3 template
+ */
+?>
+
+<div>
+<?php if (!empty($content['area1'])): ?>
+    <div><?php print $content['area1']; ?></div>
+<?php endif; ?>
+
+<?php if (!empty($content['area2'])): ?>
+    <div><?php print $content['area2']; ?></div>
+<?php endif; ?>
+
+<?php if (!empty($content['area3'])): ?>
+    <div><?php print $content['area3']; ?></div>
+<?php endif; ?>
+</div>
+
+<?php if (!empty($content['quickfacts'])): ?>
+    <div><?php print $content['quickfacts']; ?></div>
+<?php endif; ?>
+
