20260528583fe8e5dc86949c17abc762686dc370
Tom Helmer Hansen <thelmer@thehome.dk>
2014-05-15 13:46:59 +0200

    section template renamed

---
+++ themes/custom/atp_theme/templates/layouts/atp_section/section-a3.tpl.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * @file
+ * This is the section template
+ */
+?>
+
+<div>
+<?php if (!empty($content['row1'])): ?>
+    <div><?php print $content['row1']; ?></div>
+<?php endif; ?>
+
+<?php if (!empty($content['row2'])): ?>
+    <div><?php print $content['row2']; ?></div>
+<?php endif; ?>
+
+<?php if (!empty($content['row3'])): ?>
+    <div><?php print $content['row3']; ?></div>
+<?php endif; ?>
+</div>
+
+<?php if (!empty($content['row4'])): ?>
+    <div><?php print $content['row4']; ?></div>
+<?php endif; ?>
+
+<?php if (!empty($content['row5'])): ?>
+    <div><?php print $content['row5']; ?></div>
+<?php endif; ?>
