08ef61d171afa3baff4d5b97674a7fc4b499d7ca
Tom Helmer Hansen <thelmer@thehome.dk>
2014-05-15 14:25:50 +0200

    update the section layout

--- themes/custom/atp_theme/templates/layouts/atp_section/atp-section.tpl.php
+++ themes/custom/atp_theme/templates/layouts/atp_section/atp-section.tpl.php
@@ -5,24 +5,22 @@
  */
 ?>
 
-<div>
 <?php if (!empty($content['row1'])): ?>
-    <div><?php print $content['row1']; ?></div>
+  <div class="section-row"><?php print $content['row1']; ?></div>
 <?php endif; ?>
 
 <?php if (!empty($content['row2'])): ?>
-    <div><?php print $content['row2']; ?></div>
+  <div class="section-row"><?php print $content['row2']; ?></div>
 <?php endif; ?>
 
 <?php if (!empty($content['row3'])): ?>
-    <div><?php print $content['row3']; ?></div>
+  <div class="section-row"><?php print $content['row3']; ?></div>
 <?php endif; ?>
-</div>
 
 <?php if (!empty($content['row4'])): ?>
-    <div><?php print $content['row4']; ?></div>
+  <div class="section-row"><?php print $content['row4']; ?></div>
 <?php endif; ?>
 
 <?php if (!empty($content['row5'])): ?>
-    <div><?php print $content['row5']; ?></div>
+  <div class="section-row"><?php print $content['row5']; ?></div>
 <?php endif; ?>
