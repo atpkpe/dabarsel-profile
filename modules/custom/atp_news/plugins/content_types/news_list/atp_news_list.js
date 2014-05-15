6a69930c07713d5aaa839a14bb5a328b566a10a2
mikkel <mikkel@adapt.dk>
2014-05-16 00:43:17 +0200

    Adding news list content pane

---
+++ modules/custom/atp_news/plugins/content_types/news_list/atp_news_list.js
@@ -0,0 +1,39 @@
+/**
+ * Created by mikkel on 5/15/14.
+ */
+
+(function ($) {
+  Drupal.behaviors.atp_news_list = {
+    attach: function(context, settings) {
+      var viewsDomId = settings.atp_news_list.view_dom_id;
+      var instanceSettings = settings.atp_news_list.views.ajaxViews[viewsDomId];
+
+      $('.news-filter-button', context).once('filter-button').click(function(){
+
+        $(this).toggleClass('active');
+        instanceSettings.tid = {};
+        $(".news-filter-button", context).each(function() {
+          if ($(this).hasClass('active')) {
+            var tidInput = $(this).data("tid");
+            instanceSettings.tid[tidInput] = tidInput;;
+          }
+        });
+
+        $.ajax({
+          url: Drupal.settings.basePath + 'views/ajax',
+          type: 'post',
+          data: instanceSettings,
+          dataType: 'json',
+          success: function (xhr) {
+            if (xhr[2] !== undefined) {
+              var response = xhr[2];
+              $(response.selector)[response.method](response.data);
+              Drupal.behaviors.atp_news_list.attach(context, settings);
+            }
+          }
+        });
+      });
+
+    }
+  };
+})(jQuery, Drupal);
