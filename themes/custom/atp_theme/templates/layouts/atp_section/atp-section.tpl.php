cd07891d71dee0e53c76825296f3745328a9d312
Tom Helmer Hansen <thelmer@thehome.dk>
2014-05-19 22:10:14 +0200

    page updated

--- themes/custom/atp_theme/templates/layouts/atp_section/atp-section.tpl.php
+++ themes/custom/atp_theme/templates/layouts/atp_section/atp-section.tpl.php
@@ -1,26 +1,30 @@
 <?php
 /**
  * @file
- * This is the section template
+ * This is the one column template
  */
 ?>
 
+<div class="one-column clearfix" <?php if (!empty($css_id)) { print "id=\"$css_id\""; } ?>>
+
 <?php if (!empty($content['row1'])): ?>
-  <div class="section-row"><?php print $content['row1']; ?></div>
+  <div class="section-row clearfix"><?php print $content['row1']; ?></div>
 <?php endif; ?>
 
 <?php if (!empty($content['row2'])): ?>
-  <div class="section-row"><?php print $content['row2']; ?></div>
+  <div class="section-row clearfix"><?php print $content['row2']; ?></div>
 <?php endif; ?>
 
 <?php if (!empty($content['row3'])): ?>
-  <div class="section-row"><?php print $content['row3']; ?></div>
+  <div class="section-row clearfix"><?php print $content['row3']; ?></div>
 <?php endif; ?>
 
 <?php if (!empty($content['row4'])): ?>
-  <div class="section-row"><?php print $content['row4']; ?></div>
+  <div class="section-row clearfix"><?php print $content['row4']; ?></div>
 <?php endif; ?>
 
 <?php if (!empty($content['row5'])): ?>
-  <div class="section-row"><?php print $content['row5']; ?></div>
+  <div class="section-row clearfix"><?php print $content['row5']; ?></div>
 <?php endif; ?>
+
+</div>
