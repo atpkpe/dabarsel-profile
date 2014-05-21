9dab6502cf474ba67b5b136c9473d79953190f89
Frank hansen <frank@adapt.dk>
2014-05-21 14:36:48 +0200

    ATP-219

--- themes/custom/atp_theme/templates/layouts/atp_twocol/atp-twocol.tpl.php
+++ themes/custom/atp_theme/templates/layouts/atp_twocol/atp-twocol.tpl.php
@@ -5,32 +5,25 @@
  *
  */
 ?>
-<div class="twocol clearfix" <?php if (!empty($css_id)) { print "id=\"$css_id\""; } ?>>
-
-  <div class="left-col">
+<section class="twocol-left" <?php if (!empty($css_id)) { print "id=\"$css_id\""; } ?>>
+  <aside>
     <?php print $content['left']; ?>
-  </div>
-
-  <div class="right-col">
-		<?php if (!empty($content['row1'])): ?>
-		  <div class="section-row"><?php print $content['row1']; ?></div>
-		<?php endif; ?>
-
-		<?php if (!empty($content['row2'])): ?>
-		  <div class="section-row"><?php print $content['row2']; ?></div>
-		<?php endif; ?>
-
-		<?php if (!empty($content['row3'])): ?>
-		  <div class="section-row"><?php print $content['row3']; ?></div>
-		<?php endif; ?>
-
-		<?php if (!empty($content['row4'])): ?>
-		  <div class="section-row"><?php print $content['row4']; ?></div>
-		<?php endif; ?>
-
-		<?php if (!empty($content['row5'])): ?>
-		  <div class="section-row"><?php print $content['row5']; ?></div>
-		<?php endif; ?>
-  </div>
-
+  </aside >
+  <article>
+    <?php if (!empty($content['row1'])): ?>
+      <div class="section-row"><?php print $content['row1']; ?></div>
+    <?php endif; ?>
+    <?php if (!empty($content['row2'])): ?>
+      <div class="section-row"><?php print $content['row2']; ?></div>
+    <?php endif; ?>
+    <?php if (!empty($content['row3'])): ?>
+      <div class="section-row"><?php print $content['row3']; ?></div>
+    <?php endif; ?>
+    <?php if (!empty($content['row4'])): ?>
+      <div class="section-row"><?php print $content['row4']; ?></div>
+    <?php endif; ?>
+    <?php if (!empty($content['row5'])): ?>
+      <div class="section-row"><?php print $content['row5']; ?></div>
+    <?php endif; ?>
+  </article>
 </div>
