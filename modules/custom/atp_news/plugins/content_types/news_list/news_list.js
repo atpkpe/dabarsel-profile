933185bc98f09f5a50bd101babafd53480ab1e45
mikkel <mikkel@adapt.dk>
2014-05-23 14:49:46 +0200

    Adding news list view and content pane

--- modules/custom/atp_news/plugins/content_types/news_list/news_list.js
+++ modules/custom/atp_news/plugins/content_types/news_list/news_list.js
@@ -7,9 +7,8 @@
     attach: function(context, settings) {
       var viewsDomId = settings.atp_news_list.view_dom_id;
       var instanceSettings = settings.atp_news_list.views.ajaxViews[viewsDomId];
-
+      // Category click event listener.
       $('.news-filter-button', context).once('filter-button').click(function(){
-
         $(this).toggleClass('active');
         instanceSettings.tid = {};
         $(".news-filter-button", context).each(function() {
@@ -18,7 +17,7 @@
             instanceSettings.tid[tidInput] = tidInput;;
           }
         });
-
+        // Update news list view.
         $.ajax({
           url: Drupal.settings.basePath + 'views/ajax',
           type: 'post',
@@ -27,11 +26,13 @@
           success: function (xhr) {
             if (xhr[2] !== undefined) {
               var response = xhr[2];
+              console.log(response);
               $(response.selector)[response.method](response.data);
-              Drupal.behaviors.atp_news_list.attach(context, settings);
+              Drupal.attachBehaviors(context, settings);
             }
           }
         });
+
       });
 
     }
