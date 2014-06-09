5ba0deae6bccf25fc018aa5d0aca890b5a579304
Tom Helmer Hansen <thelmer@thehome.dk>
2014-06-09 19:42:45 +0200

    arkena integration

---
+++ modules/custom/media_qbrick/includes/media-qbrick-video.tpl.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Qbrick video here
+ * - $video_id
+ * - $options
+ */
+?>
+
+<div data-qbrick-mcid="<?php print $video_id; ?>">&nbsp;</div>
+<script type="text/javascript" src="https://publisher.qbrick.com/Qbrick.Publisher.js"></script>
