b9b48136754d3c04b616f512b3311d34cbb1c79d
Max Andersen <max@adapt.dk>
2014-07-30 14:20:54 +0200

    Styling for webforms and mailchimp form

--- modules/custom/atp_signup/tpl/signup.tpl.php
+++ modules/custom/atp_signup/tpl/signup.tpl.php
@@ -1,3 +1,34 @@
 <div class="signup">
+<!-- Begin MailChimp Signup Form -->
+	<div id="mc_embed_signup">
+	<form action="//atp.us8.list-manage.com/subscribe/post?u=06eff39be00dbfa57deb5d09a&amp;id=9814adf0d7" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
+		
+	<div class="mc-field-group">
+		<label for="mce-EMAIL">E-mail adresse <span class="form-required" title="Dette felt er obligatorisk.">*</span></label>
+		<input type="email" value="" name="EMAIL" class="required email form-text" id="mce-EMAIL">
+	</div>
+	<div class="mc-field-group">
+		<label for="mce-FNAME">Navn <span class="form-required" title="Dette felt er obligatorisk.">*</span></label>
+		<input type="text" value="" name="FNAME" class="required form-text" id="mce-FNAME">
+	</div>
+	<div class="mc-field-group input-group">
+	    <h2>Vælg emner </h2>
+	    <ul><li><input type="checkbox" value="1" name="group[12817][1]" id="mce-group[12817]-12817-0"><label for="mce-group[12817]-12817-0">Årsrapporter og regnskabmeddelelser</label></li>
+	<li><input type="checkbox" value="2" name="group[12817][2]" id="mce-group[12817]-12817-1"><label for="mce-group[12817]-12817-1">Emne 02</label></li>
+	<li><input type="checkbox" value="4" name="group[12817][4]" id="mce-group[12817]-12817-2"><label for="mce-group[12817]-12817-2">Emne 03</label></li>
+	<li><input type="checkbox" value="8" name="group[12817][8]" id="mce-group[12817]-12817-3"><label for="mce-group[12817]-12817-3">Emne 04</label></li>
+	<li><input type="checkbox" value="16" name="group[12817][16]" id="mce-group[12817]-12817-4"><label for="mce-group[12817]-12817-4">Emne 05</label></li>
+	<li><input type="checkbox" value="32" name="group[12817][32]" id="mce-group[12817]-12817-5"><label for="mce-group[12817]-12817-5">Emne 06</label></li>
+	</ul>
+	</div>
+		<div id="mce-responses" class="clear">
+			<div class="response" id="mce-error-response" style="display:none"></div>
+			<div class="response" id="mce-success-response" style="display:none"></div>
+		</div>    <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
+	    <div style="position: absolute; left: -5000px;"><input type="text" name="b_06eff39be00dbfa57deb5d09a_9814adf0d7" tabindex="-1" value=""></div>
+	    <div class="clear"><input type="submit" value="Tilmeld" name="subscribe" id="mc-embedded-subscribe" class="button form-submit"></div>
+	</form>
+	</div>
 
+<!--End mc_embed_signup-->
 </div>
