6796fd868b7fcf1ece4eb0d0c85e805a8b6db6e1
Tom Helmer Hansen <tom@adapt.dk>
2014-09-03 13:08:03 +0200

    Important message

--- modules/custom/atp_important_message/js/important_message.js
+++ modules/custom/atp_important_message/js/important_message.js
@@ -2,25 +2,25 @@
   Drupal.behaviors.important_message = {
     attach: function (context, settings) {
 
-      $('#important-message', context).click(function() {
+      $('#important-message div.close', context).click(function() {
         $.cookie('important_message', Drupal.settings.important_message_id, {expires:1000, path:'/'});
         $('#important-message').hide();
       });
 
       $('#important-message', context).once('important-message', function() {
-
-        var important_message = 0;
-        if ($.cookie('important_message') !== undefined) {
-          important_message = $.cookie('important_message');
+        var messageId = 0;
+        if ($.cookie('important_message')) {
+          messageId = $.cookie('important_message');
         }
-
-        $.post("js/atp_important_message/results/" + important_message, function( data ) {
+        $.post(Drupal.settings.pathPrefix + "js/atp_important_message/results/" + messageId, function( data ) {
           if (data.show_message) {
-            $('#important-message').html( data.title );
-            Drupal.settings.important_message_id = data.important_message_id;
+            $('#important-message div.content').html( data.html );
+            Drupal.settings.important_message_id = data.message_id;
+            $('#important-message').show();
           }
         });
       });
+
     }
   };
 })(jQuery);
