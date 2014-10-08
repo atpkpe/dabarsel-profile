486dbc5e150b42e57cecc826a778f2cda4ad0fff
Max Andersen <max@adapt.dk>
2014-10-08 13:59:25 +0200

    [ATP-202] Updated newsletter signup form

--- modules/custom/atp_signup/tpl/signup.tpl.php
+++ modules/custom/atp_signup/tpl/signup.tpl.php
@@ -5,28 +5,35 @@
     <div id="mc_embed_signup_scroll">
 	
 <div class="mc-field-group">
-	<label for="mce-EMAIL">E-mail adresse  <span class="asterisk">*</span>
+	<label for="mce-EMAIL"><?php print t("E-mail address"); ?> <span class="asterisk">*</span>
 </label>
 	<input type="email" value="" name="EMAIL" class="required email form-text" id="mce-EMAIL">
 </div>
 <div class="mc-field-group">
-	<label for="mce-FNAME">Fornavn </label>
+	<label for="mce-FNAME"><?php print t("First name"); ?> </label>
 	<input type="text" value="" name="FNAME" class="form-text" id="mce-FNAME">
 </div>
 <div class="mc-field-group">
-	<label for="mce-LNAME">Efternavn </label>
+	<label for="mce-LNAME"><?php print t("Last name"); ?> </label>
 	<input type="text" value="" name="LNAME" class="form-text" id="mce-LNAME">
 </div>
 <div class="mc-field-group input-group">
-    <strong>Emner </strong>
-    <ul><li><input type="checkbox" value="1" name="group[15001][1]" id="mce-group[15001]-15001-0"><label for="mce-group[15001]-15001-0">Faktum</label></li>
-<li><input type="checkbox" value="2" name="group[15001][2]" id="mce-group[15001]-15001-1"><label for="mce-group[15001]-15001-1">Nyt om investering</label></li>
-<li><input type="checkbox" value="4" name="group[15001][4]" id="mce-group[15001]-15001-2"><label for="mce-group[15001]-15001-2">Nyt om pension</label></li>
-<li><input type="checkbox" value="8" name="group[15001][8]" id="mce-group[15001]-15001-3"><label for="mce-group[15001]-15001-3">Nyt om job</label></li>
-<li><input type="checkbox" value="16" name="group[15001][16]" id="mce-group[15001]-15001-4"><label for="mce-group[15001]-15001-4">Nyt om Udbetaling Danmark</label></li>
-<li><input type="checkbox" value="32" name="group[15001][32]" id="mce-group[15001]-15001-5"><label for="mce-group[15001]-15001-5">Udbud fra Udbetaling Danmark</label></li>
-<li><input type="checkbox" value="64" name="group[15001][64]" id="mce-group[15001]-15001-6"><label for="mce-group[15001]-15001-6">Pressemeddelelser</label></li>
-<li><input type="checkbox" value="128" name="group[15001][128]" id="mce-group[15001]-15001-8"><label for="mce-group[15001]-15001-8">Resultater og rapporter</label></li>
+    <h2><?php print t("Topics"); ?> </h2>
+    <ul>
+    <!-- Danish -->
+    <?php if($lang == "da"): ?>
+			<li><input type="checkbox" value="1" name="group[15001][1]" id="mce-group[15001]-15001-0"><label for="mce-group[15001]-15001-0"><?php print t("Facts"); ?></label></li>
+			<li><input type="checkbox" value="2" name="group[15001][2]" id="mce-group[15001]-15001-1"><label for="mce-group[15001]-15001-1"><?php print t("Investment"); ?></label></li>
+			<li><input type="checkbox" value="4" name="group[15001][4]" id="mce-group[15001]-15001-2"><label for="mce-group[15001]-15001-2"><?php print t("Retirement"); ?></label></li>
+			<li><input type="checkbox" value="8" name="group[15001][8]" id="mce-group[15001]-15001-3"><label for="mce-group[15001]-15001-3"><?php print t("Jobs"); ?></label></li>
+			<li><input type="checkbox" value="16" name="group[15001][16]" id="mce-group[15001]-15001-4"><label for="mce-group[15001]-15001-4"><?php print t("Udbetaling Danmark news"); ?></label></li>
+			<li><input type="checkbox" value="32" name="group[15001][32]" id="mce-group[15001]-15001-5"><label for="mce-group[15001]-15001-5"><?php print t("Contracts from Udbetaling Danmark"); ?></label></li>
+			<li><input type="checkbox" value="64" name="group[15001][64]" id="mce-group[15001]-15001-6"><label for="mce-group[15001]-15001-6"><?php print t("Press releases"); ?></label></li>
+			<li><input type="checkbox" value="128" name="group[15001][128]" id="mce-group[15001]-15001-8"><label for="mce-group[15001]-15001-8"><?php print t("Results and reports"); ?></label></li>
+		<?php elseif($lang == "en"): ?>
+			<!-- English -->
+			<li><input type="checkbox" value="256" name="group[15001][256]" id="mce-group[15001]-15001-7"><label for="mce-group[15001]-15001-7"><?php print t("Press releases"); ?></label></li>
+		<?php endif; ?>
 </ul>
 </div>
 	<div id="mce-responses" class="clear">
@@ -34,7 +41,7 @@
 		<div class="response" id="mce-success-response" style="display:none"></div>
 	</div>    <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
     <div style="position: absolute; left: -5000px;"><input type="text" name="b_06eff39be00dbfa57deb5d09a_9bc98b0cb5" tabindex="-1" value=""></div>
-    <div class="clear"><input type="submit" value="Subscribe" name="subscribe" id="mc-embedded-subscribe" class="button form-submit"></div>
+    <div class="clear"><input type="submit" value="<?php print t('Signup'); ?>" name="subscribe" id="mc-embedded-subscribe" class="button form-submit"></div>
     </div>
 </form>
 </div>
